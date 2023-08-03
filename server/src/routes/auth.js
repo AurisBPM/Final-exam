513c60a93c64608a47d8dcbf70399eb106d1b864
AurisBPM <124934947+AurisBPM@users.noreply.github.com>
2023-08-03 23:06:58 +0300

    Auth registration

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
+const joi = require('joi');
+const bcrypt = require('bcrypt');
+const DB_CONFIG = require('../config/db-config');
 
 const router = express.Router();
+const mysqlPool = mysql.createPool(DB_CONFIG).promise();
+
+const authSchema = joi.object({
+  email: joi.string().email().trim().lowercase()
+    .required(),
+  password: joi.string()
+    .required(),
+});
 
 router.post('/register', async (req, res) => {
-  const payload = req.body;
+  let payload = req.body;
+  console.log(payload);
   try {
-    console.log(payload);
+    payload = await authSchema.validateAsync(payload);
   } catch (err) {
     return res.status(400).send({ error: 'All fields are required' });
   }
   try {
-    return res.status(200).end();
+    const encryptedPassword = await bcrypt.hash(payload.password, 10);
+    const [response] = await mysqlPool.execute('INSERT INTO auth ( email, password ) VALUES (?, ? )', [payload.email, encryptedPassword]);
+    return res.status(200).json(response);
   } catch (err) {
     if (err.code === 'ER_DUP_ENTRY') {
       return res.status(400).json({ error: 'Duplicate entry' });
